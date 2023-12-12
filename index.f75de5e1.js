113285a2e0a3703ee7061482641ca593dd3b51ab
Nataliia Henda <nataliehenda1296@gmail.com>
2023-12-12 17:42:42 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderlWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+spider.style.position = "relative";
+spider.style.top = `${wallHeight / 2 - spiderHeight / 2}px`;
+spider.style.left = `${wallWidth / 2 - spiderlWidth / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
